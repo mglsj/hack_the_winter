b6a12c683557e39a7e66d2ffb5044639c961a342
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-11-08 20:54:12 +0530

    domain transfer

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hack-the-winter.pages.dev",
+	site: "https://hack.gehubhimtal.in/",
 	integrations: [mdx(), lenis(), icon(), react()],
 	vite: {
 		plugins: [tailwindcss()],
